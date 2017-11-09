c92b758a4d3931cafe727e9c6d60012500bd3a55
lawr <lawrance.rsp@gmail.com>
2017-11-10 02:20:22 +0800

    修改index位置

--- index.js
+++
@@ -1,4 +0,0 @@
-export { default as reduxForm } from './src/reduxForm';
-export { default as reducer } from './src/reducer';
-export * from './src/actions';
-
---
+++ src/index.js
@@ -0,0 +1,4 @@
+export { default as reduxForm } from './reduxForm';
+export { default as reducer } from './reducer';
+export * from './actions';
+
